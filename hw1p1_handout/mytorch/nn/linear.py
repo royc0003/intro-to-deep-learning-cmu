669745134c1e0b23e9b082777c9a6dc33e895787
Younseo+Royce <royceang96@gmail.com>
2025-08-31 20:44:40 -0400

    first pass

--- hw1p1_handout/mytorch/nn/linear.py
+++ hw1p1_handout/mytorch/nn/linear.py
@@ -6,10 +6,14 @@ class Linear:
         """
         Initialize the weights and biases with zeros (Hint: check np.zeros method)
         Read the writeup (Hint: Linear Layer Section Table) to identify the right shapes for `W` and `b`.
+        in_features (C_in): number of input features - 2
+        out_features (C_out): number of output features - 3
         """
         self.debug = debug
-        self.W = None  # TODO
-        self.b = None  # TODO
+        # W = C_out x C_in
+        self.W = np.zeros((out_features, in_features))
+        # b = C_out x 1
+        self.b = np.zeros((out_features, 1))
 
     def forward(self, A):
         """
@@ -18,14 +22,14 @@ class Linear:
 
         Read the writeup (Hint: Linear Layer Section) for implementation details for `Z`
         """
-        self.A = None  # TODO
-        self.N = None  # TODO - store the batch size parameter of the input A
+        self.A = A
+        self.N = self.A.shape[0]  # store the batch size parameter of the input A
 
         # Think how can `self.ones` help in the calculations and uncomment below code snippet.
-        # self.ones = np.ones((self.N, 1))
-
-        Z = None  # TODO
-        raise NotImplemented  # TODO - What should be the return value?
+        self.ones = np.ones((self.N, 1))
+        
+        Z = self.A @ self.W.T + self.ones @ self.b.T
+        return Z
 
     def backward(self, dLdZ):
         """
